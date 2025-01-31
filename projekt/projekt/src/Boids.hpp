067b53122076453be76dda3714ad1a02b74b55b9
MarRac <marekraczkowski5@gmail.com>
2025-01-31 03:32:59 +0100

    Add project files.

---
+++ projekt/projekt/src/Boids.hpp
@@ -0,0 +1,189 @@
+﻿#include "glew.h"
+#include <GLFW/glfw3.h>
+#include "glm.hpp"
+#include "ext.hpp"
+#include <iostream>
+#include <cmath>
+
+#include "Shader_Loader.h"
+#include "Render_Utils.h"
+//#include "Texture.h"
+
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+#include <string>
+
+#include <cstdlib>
+#include <ctime> 
+#include <vector>
+
+int randomInt(int min, int max) {
+	return min + rand() % (max - min + 1);
+}
+
+
+class Boid {
+public:
+	glm::vec3 position;
+	glm::vec3 velocity;  // (szybkość + kierunek)
+	int groupId;
+
+	float maxSpeed = 0.04f;
+	float maxForce = 0.2f;
+
+	float NEIGHBOUR_DISTANCE = 2.f;
+	float AVOID_RADIUS = 0.5f;
+
+	float ALIGMENT_WEIGHT = 1.0f;
+	float COHESION_WEIGHT = 0.001f;
+	float SEPARATION_WEIGHT = 2.0f;
+
+	float minX = -4.6f, maxX = 4.6f;
+	float minY = 0.5f, maxY = 5.0f;
+	float minZ = -1.6f, maxZ = 1.6f;
+
+
+	Boid(glm::vec3 startPosition) : position(startPosition) {
+		groupId = randomInt(0, 3);
+		velocity = glm::vec3(0.01f);
+	}
+
+	glm::vec3 getGroupColor() {
+		switch (groupId) {
+		case 0: return glm::vec3(1.0f, 0.0f, 0.0f);  // grupa 1 --> czerwony
+		case 1: return glm::vec3(0.0f, 1.0f, 0.0f);  // grupa 2 --> zielony
+		case 2: return glm::vec3(0.0f, 0.0f, 1.0f);  // grupa 3 --> niebieski
+		case 3: return glm::vec3(1.0f, 1.0f, 0.0f);  // grupa 4 --> żółty
+		default: return glm::vec3(1.0f, 0.0f, 0.0f);
+		}
+	}
+
+
+
+	void applyBoundaryForce() {
+		glm::vec3 boundaryForce(0.0f);
+
+		if (position.x > maxX) {
+			boundaryForce.x = -maxForce;
+		}
+		else if (position.x < minX) {
+			boundaryForce.x = maxForce;
+		}
+
+		if (position.y > maxY) {
+			boundaryForce.y = -maxForce;
+		}
+		else if (position.y < minY) {
+			boundaryForce.y = maxForce;
+		}
+
+		if (position.z > maxZ) {
+			boundaryForce.z = -maxForce;
+		}
+		else if (position.z < minZ) {
+			boundaryForce.z = maxForce;
+		}
+		velocity += boundaryForce;
+	}
+
+
+
+	glm::vec3 separate(std::vector<Boid*>& boids) {
+		glm::vec3 avoid_vector(0.0f);
+		float distance;
+
+		for (Boid* other_boid : boids) {
+			if (other_boid->groupId == groupId) {
+				distance = glm::distance(position, other_boid->position);
+				if (distance < AVOID_RADIUS) {
+					if (distance != 0) {
+						avoid_vector += glm::normalize(position - other_boid->position) / distance;
+					}
+				}
+			}
+		}
+		return avoid_vector;
+	}
+
+	glm::vec3 align(std::vector<Boid*>& boids) {
+
+		glm::vec3 avg_boid_vector(0.0f);
+		int count = 0;
+		float distance;
+
+		for (Boid* other_boid : boids) {
+			if (other_boid->groupId == groupId) {
+				distance = glm::distance(position, other_boid->position);
+				if (distance <= NEIGHBOUR_DISTANCE) {
+					avg_boid_vector += other_boid->velocity;
+					count++;
+				}
+			}
+		}
+		if (count > 0) {
+			avg_boid_vector = avg_boid_vector / count;
+			avg_boid_vector = glm::normalize(avg_boid_vector) * maxSpeed;
+		}
+		return avg_boid_vector - velocity;
+	}
+
+
+
+	glm::vec3 cohesion(std::vector<Boid*>& boids) {
+		glm::vec3  center_of_group(0.0f);
+		int count = 0;
+		float distance;
+		for (Boid* other_boid : boids) {
+			if (other_boid->groupId == groupId) {
+				distance = glm::distance(position, other_boid->position);
+				if (distance <= NEIGHBOUR_DISTANCE) {
+					center_of_group += other_boid->position;
+					count++;
+				}
+			}
+		}
+
+		if (count > 0) {
+			center_of_group = center_of_group / count;
+			return glm::normalize(center_of_group - position);
+		}
+		return glm::vec3(0.0f);
+	}
+
+
+	void update(std::vector<Boid*>& boids) {
+
+		glm::vec3 _separation = separate(boids) * SEPARATION_WEIGHT;
+		glm::vec3 _aligment = align(boids) * ALIGMENT_WEIGHT;
+		glm::vec3 _cohesion = cohesion(boids) * COHESION_WEIGHT;
+
+		glm::vec3 steering = _separation + _aligment + _cohesion;
+		velocity += steering;
+
+		if (glm::length(velocity) > maxSpeed) {
+			velocity = glm::normalize(velocity) * maxSpeed;
+		}
+		applyBoundaryForce();
+		position += velocity;
+	}
+
+
+
+	glm::mat4 getMatrix() {
+		glm::vec3 direction = glm::normalize(velocity);
+		glm::vec3 referenceVector = glm::vec3(0, 0, 1);
+
+		glm::vec3 rotationAxis = glm::cross(referenceVector, direction);
+		float angle = acos(glm::dot(referenceVector, direction));
+
+		// rotuje model w kierunku poruszania się
+		glm::mat4 rotationMatrix = glm::rotate(glm::mat4(1.0f), angle, rotationAxis);
+
+		glm::mat4 modelMatrix = glm::translate(position) * rotationMatrix * glm::scale(glm::vec3(0.7f));
+
+		return modelMatrix;
+
+	}
+
+};
---
+++ projekt/projekt/src/projekt.hpp
@@ -0,0 +1,307 @@
+﻿#include "glew.h"
+#include <GLFW/glfw3.h>
+#include "glm.hpp"
+#include "ext.hpp"
+#include <iostream>
+#include <cmath>
+
+#include "Shader_Loader.h"
+#include "Render_Utils.h"
+//#include "Texture.h"
+
+#include "Box.cpp"
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+#include <string>
+#include "Boids.hpp"
+
+#include <cstdlib>
+#include <ctime> 
+#include <vector>
+
+const unsigned int SHADOW_WIDTH = 1024, SHADOW_HEIGHT = 1024;
+
+int WIDTH = 500, HEIGHT = 500;
+
+namespace models {
+	Core::RenderContext spaceshipContext;
+	Core::RenderContext sphereContext;
+	Core::RenderContext aquariumContext;
+	Core::RenderContext goldfishContext;
+}
+
+
+
+GLuint depthMapFBO;
+GLuint depthMap;
+
+GLuint program;
+
+Core::Shader_Loader shaderLoader;
+
+Core::RenderContext shipContext;
+Core::RenderContext sphereContext;
+Core::RenderContext goldfishContext;
+
+glm::vec3 sunPos = glm::vec3(-4.740971f, 2.149999f, 0.369280f);
+glm::vec3 sunDir = glm::vec3(-0.93633f, 0.351106, 0.003226f);
+glm::vec3 sunColor = glm::vec3(0.9f, 0.9f, 0.7f) * 0.5;
+
+glm::vec3 cameraPos = glm::vec3(0.479490f, 1.250000f, -2.124680f);
+glm::vec3 cameraDir = glm::vec3(-0.354510f, 0.000000f, 0.935054f);
+
+
+glm::vec3 spaceshipPos = glm::vec3(0.065808f, 1.250000f, -2.189549f);
+glm::vec3 spaceshipDir = glm::vec3(-0.490263f, 0.000000f, 0.871578f);
+GLuint VAO, VBO;
+
+float aspectRatio = 1.f;
+
+float exposition = 1.f;
+
+glm::vec3 pointlightPos = glm::vec3(0, 4, 0);
+glm::vec3 pointlightColor = glm::vec3(0.9, 0.6, 0.6);
+
+glm::vec3 spotlightPos = glm::vec3(0, 0, 0);
+glm::vec3 spotlightConeDir = glm::vec3(0, 0, 0);
+glm::vec3 spotlightColor = glm::vec3(0.4, 0.4, 0.9) * 3;
+float spotlightPhi = 3.14 / 4;
+
+
+std::vector<Boid*> boids;
+
+float lastTime = -1.f;
+float deltaTime = 0.f;
+
+
+void updateDeltaTime(float time) {
+	if (lastTime < 0) {
+		lastTime = time;
+		return;
+	}
+	deltaTime = time - lastTime;
+	if (deltaTime > 0.1) deltaTime = 0.1;
+	lastTime = time;
+}
+
+
+
+glm::mat4 createCameraMatrix()
+{
+	glm::vec3 cameraSide = glm::normalize(glm::cross(cameraDir, glm::vec3(0.f, 1.f, 0.f)));
+	glm::vec3 cameraUp = glm::normalize(glm::cross(cameraSide, cameraDir));
+	glm::mat4 cameraRotrationMatrix = glm::mat4({
+		cameraSide.x,cameraSide.y,cameraSide.z,0,
+		cameraUp.x,cameraUp.y,cameraUp.z ,0,
+		-cameraDir.x,-cameraDir.y,-cameraDir.z,0,
+		0.,0.,0.,1.,
+		});
+	cameraRotrationMatrix = glm::transpose(cameraRotrationMatrix);
+	glm::mat4 cameraMatrix = cameraRotrationMatrix * glm::translate(-cameraPos);
+	return cameraMatrix;
+}
+
+
+glm::mat4 createPerspectiveMatrix()
+{
+	glm::mat4 perspectiveMatrix;
+	float n = 0.05;
+	float f = 20.;
+	float a1 = glm::min(aspectRatio, 1.f);
+	float a2 = glm::min(1 / aspectRatio, 1.f);
+	perspectiveMatrix = glm::mat4({
+		1,0.,0.,0.,
+		0.,aspectRatio,0.,0.,
+		0.,0.,(f + n) / (n - f),2 * f * n / (n - f),
+		0.,0.,-1.,0.,
+		});
+	perspectiveMatrix = glm::transpose(perspectiveMatrix);
+	return perspectiveMatrix;
+}
+
+
+
+void drawObjectPBR(Core::RenderContext& context, glm::mat4 modelMatrix, glm::vec3 color, float roughness, float metallic) {
+
+	glm::mat4 viewProjectionMatrix = createPerspectiveMatrix() * createCameraMatrix();
+	glm::mat4 transformation = viewProjectionMatrix * modelMatrix;
+	glUniformMatrix4fv(glGetUniformLocation(program, "transformation"), 1, GL_FALSE, (float*)&transformation);
+	glUniformMatrix4fv(glGetUniformLocation(program, "modelMatrix"), 1, GL_FALSE, (float*)&modelMatrix);
+
+	glUniform1f(glGetUniformLocation(program, "exposition"), exposition);
+
+	glUniform1f(glGetUniformLocation(program, "roughness"), roughness);
+	glUniform1f(glGetUniformLocation(program, "metallic"), metallic);
+
+	glUniform3f(glGetUniformLocation(program, "color"), color.x, color.y, color.z);
+
+	glUniform3f(glGetUniformLocation(program, "cameraPos"), cameraPos.x, cameraPos.y, cameraPos.z);
+
+	glUniform3f(glGetUniformLocation(program, "sunDir"), sunDir.x, sunDir.y, sunDir.z);
+	glUniform3f(glGetUniformLocation(program, "sunColor"), sunColor.x, sunColor.y, sunColor.z);
+
+	glUniform3f(glGetUniformLocation(program, "lightPos"), pointlightPos.x, pointlightPos.y, pointlightPos.z);
+	glUniform3f(glGetUniformLocation(program, "lightColor"), pointlightColor.x, pointlightColor.y, pointlightColor.z);
+
+	glUniform3f(glGetUniformLocation(program, "spotlightConeDir"), spotlightConeDir.x, spotlightConeDir.y, spotlightConeDir.z);
+	glUniform3f(glGetUniformLocation(program, "spotlightPos"), spotlightPos.x, spotlightPos.y, spotlightPos.z);
+	glUniform3f(glGetUniformLocation(program, "spotlightColor"), spotlightColor.x, spotlightColor.y, spotlightColor.z);
+	glUniform1f(glGetUniformLocation(program, "spotlightPhi"), spotlightPhi);
+	Core::DrawContext(context);
+
+}
+
+
+void renderShadowapSun() {
+	float time = glfwGetTime();
+	glViewport(0, 0, SHADOW_WIDTH, SHADOW_HEIGHT);
+
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+	glViewport(0, 0, WIDTH, HEIGHT);
+}
+
+
+
+void renderScene(GLFWwindow* window)
+{
+	glClearColor(0.4f, 0.4f, 0.8f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	float time = glfwGetTime();
+	updateDeltaTime(time);
+	renderShadowapSun();
+
+	glUseProgram(program);
+
+
+	drawObjectPBR(models::aquariumContext, glm::mat4() * glm::scale(glm::vec3(0.3)) * glm::rotate(glm::radians(-90.0f), glm::vec3(1.0f, 0.0f, 0.0f)),
+		glm::vec3(1.f), 0.2, 1.0);
+
+	for (Boid* b : boids) {
+		b->update(boids);
+		drawObjectPBR(models::goldfishContext, b->getMatrix(), b->getGroupColor(), 0.2, 1.0);
+	}
+
+	glUseProgram(0);
+	glfwSwapBuffers(window);
+}
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	aspectRatio = width / float(height);
+	glViewport(0, 0, width, height);
+	WIDTH = width;
+	HEIGHT = height;
+}
+
+
+void loadModelToContext(std::string path, Core::RenderContext& context)
+{
+	Assimp::Importer import;
+	const aiScene * scene = import.ReadFile(path, aiProcess_Triangulate | aiProcess_CalcTangentSpace);
+
+	if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode)
+	{
+		std::cout << "ERROR::ASSIMP::" << import.GetErrorString() << std::endl;
+		return;
+	}
+	context.initFromAssimpMesh(scene->mMeshes[0]);
+}
+
+
+
+float randomFloat(float min, float max) {
+	return min + static_cast<float>(rand()) / static_cast<float>(RAND_MAX) * (max - min);
+}
+
+
+glm::vec3 randomVec3() {
+	return glm::vec3(
+		randomFloat(-4.6f, 4.6),
+		randomFloat(0.5f, 5.0f),
+		randomFloat(-1.6f, 1.6f)
+	);
+}
+
+
+void init(GLFWwindow* window)
+{
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	glEnable(GL_DEPTH_TEST);
+	program = shaderLoader.CreateProgram("shaders/shader_9_1.vert", "shaders/shader_9_1.frag");
+
+	loadModelToContext("./models/sphere.obj", sphereContext);
+	loadModelToContext("./models/spaceship.obj", shipContext);
+
+	loadModelToContext("./models/spaceship.obj", models::spaceshipContext);
+	loadModelToContext("./models/sphere.obj", models::sphereContext);
+	loadModelToContext("./models/aquarium.obj", models::aquariumContext);
+
+	loadModelToContext("./models/goldie.obj", models::goldfishContext);
+
+	for (int i = 0; i <= 140; i++) {
+		boids.push_back(new Boid(randomVec3()));
+	}
+
+
+}
+void shutdown(GLFWwindow* window)
+{
+	shaderLoader.DeleteProgram(program);
+}
+
+
+
+void processInput(GLFWwindow* window)
+{
+	glm::vec3 spaceshipSide = glm::normalize(glm::cross(spaceshipDir, glm::vec3(0.f, 1.f, 0.f)));
+	glm::vec3 spaceshipUp = glm::vec3(0.f, 1.f, 0.f);
+	float angleSpeed = 0.05f * deltaTime * 60;
+	float moveSpeed = 0.05f * deltaTime * 60;
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, true);
+	}
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		spaceshipPos += spaceshipDir * moveSpeed;
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		spaceshipPos -= spaceshipDir * moveSpeed;
+	if (glfwGetKey(window, GLFW_KEY_X) == GLFW_PRESS)
+		spaceshipPos += spaceshipSide * moveSpeed;
+	if (glfwGetKey(window, GLFW_KEY_Z) == GLFW_PRESS)
+		spaceshipPos -= spaceshipSide * moveSpeed;
+	if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
+		spaceshipPos += spaceshipUp * moveSpeed;
+	if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS)
+		spaceshipPos -= spaceshipUp * moveSpeed;
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		spaceshipDir = glm::vec3(glm::eulerAngleY(angleSpeed) * glm::vec4(spaceshipDir, 0));
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		spaceshipDir = glm::vec3(glm::eulerAngleY(-angleSpeed) * glm::vec4(spaceshipDir, 0));
+
+	cameraPos = spaceshipPos - 0.5 * spaceshipDir + glm::vec3(0, 1, 0) * 0.2f;
+	cameraDir = spaceshipDir;
+
+	if (glfwGetKey(window, GLFW_KEY_1) == GLFW_PRESS)
+		exposition -= 0.05;
+	if (glfwGetKey(window, GLFW_KEY_2) == GLFW_PRESS)
+		exposition += 0.05;
+
+	if (glfwGetKey(window, GLFW_KEY_3) == GLFW_PRESS) {
+		printf("spaceshipPos = glm::vec3(%ff, %ff, %ff);\n", spaceshipPos.x, spaceshipPos.y, spaceshipPos.z);
+		printf("spaceshipDir = glm::vec3(%ff, %ff, %ff);\n", spaceshipDir.x, spaceshipDir.y, spaceshipDir.z);
+	}
+
+
+}
+
+void renderLoop(GLFWwindow* window) {
+	while (!glfwWindowShouldClose(window))
+	{
+		processInput(window);
+
+		renderScene(window);
+		glfwPollEvents();
+	}
+}
